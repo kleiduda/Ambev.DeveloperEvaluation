97b5c2ddb97bd337b472a122634586f518739269
Kleiton <kleiduda@gmail.com>
2025-04-09 13:41:04 -0300

    feat(orm): adiciona mapeamento EF Core para entidade Product com Rating embutido

---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DefaultContext _context;
+
+        public ProductRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            await _context.Products.AddAsync(product, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return product;
+        }
+    }
+
+}
