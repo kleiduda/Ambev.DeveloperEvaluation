12654df6ec1eeabd70c678c326abc25f7d4dd233
Kleiton <kleiduda@gmail.com>
2025-04-09 16:12:08 -0300

    feat(orm): adiciona configuração EF Core para Cart e seus produtos com tabela separada

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/CartConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("Carts");
+
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id)
+                   .HasColumnType("uuid")
+                   .HasDefaultValueSql("gen_random_uuid()");
+
+            builder.Property(c => c.UserId).IsRequired();
+            builder.Property(c => c.Date).IsRequired();
+
+            builder.OwnsMany(c => c.Products, pb =>
+            {
+                pb.ToTable("CartProducts");
+
+                pb.WithOwner().HasForeignKey("CartId");
+                pb.Property<Guid>("Id");
+                pb.HasKey("Id");
+
+                pb.Property(p => p.ProductId).IsRequired();
+                pb.Property(p => p.Quantity).IsRequired();
+            });
+        }
+    }
+
+}
