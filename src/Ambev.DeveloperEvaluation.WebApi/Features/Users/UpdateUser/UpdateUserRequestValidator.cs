3db7cd36ed670b96839b37afaa6fbe43c14fbc40
Kleiton <kleiduda@gmail.com>
2025-04-09 09:40:16 -0300

    feat(webapi): implementa validação de UpdateUserRequest com FluentValidation

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Users/UpdateUser/UpdateUserRequestValidator.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Users/UpdateUser/UpdateUserRequestValidator.cs
@@ -1,6 +1,23 @@
-﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.UpdateUser
+﻿using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Users.UpdateUser
 {
-    public class UpdateUserRequestValidator
+    public class UpdateUserRequestValidator : AbstractValidator<UpdateUserRequest>
     {
+        public UpdateUserRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Id is required.");
+
+            RuleFor(user => user.Email).SetValidator(new EmailValidator());
+            RuleFor(user => user.Username).NotEmpty().Length(3, 50);
+            RuleFor(user => user.Password).SetValidator(new PasswordValidator());
+            RuleFor(user => user.Phone).Matches(@"^\+?[1-9]\d{1,14}$");
+            RuleFor(user => user.Status).NotEqual(UserStatus.Unknown);
+            RuleFor(user => user.Role).NotEqual(UserRole.None);
+        }
     }
+
 }
