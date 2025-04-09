ec9c5202fbed2adcd3acf76e18f7de0df9745ed6
Kleiton <kleiduda@gmail.com>
2025-04-09 16:10:19 -0300

    feat(domain): adiciona entidade Cart com coleção de CartProducts

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Cart.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Cart : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public DateTime Date { get; set; }
+        public List<CartProduct> Products { get; set; } = new();
+    }
+
+    public class CartProduct
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
