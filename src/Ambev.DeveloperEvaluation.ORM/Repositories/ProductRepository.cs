89a591661b7ff4d671214c6363bd0ff9757a1507
Kleiton <kleiduda@gmail.com>
2025-04-09 14:19:17 -0300

    feat(infrastructure): implementa listagem paginada e ordenada de produtos

--- src/Ambev.DeveloperEvaluation.ORM/Repositories/ProductRepository.cs
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/ProductRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
 
 namespace Ambev.DeveloperEvaluation.ORM.Repositories
 {
@@ -17,6 +19,36 @@ namespace Ambev.DeveloperEvaluation.ORM.Repositories
             await _context.Products.AddAsync(product, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
             return product;
+        }
+
+        public async Task<(List<Product> Items, int TotalItems)> GetPaginatedAsync(int page, int pageSize, string? orderBy, CancellationToken cancellationToken)
+        {
+            var query = _context.Products.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(orderBy))
+            {
+                try
+                {
+                    query = query.OrderBy(orderBy); 
+                }
+                catch
+                {
+                    query = query.OrderBy(p => p.Title); 
+                }
+            }
+            else
+            {
+                query = query.OrderBy(p => p.Title);
+            }
+
+            var totalItems = await query.CountAsync(cancellationToken);
+
+            var items = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return (items, totalItems);
         }
     }
 
