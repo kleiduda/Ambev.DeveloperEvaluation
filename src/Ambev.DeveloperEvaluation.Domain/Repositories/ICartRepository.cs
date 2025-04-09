e713aac9e642851ebe5f5755fc1721f3be4a3060
Kleiton <kleiduda@gmail.com>
2025-04-09 17:17:41 -0300

    feat(application): adiciona método GetByIdAsync na interface ICartRepository

--- src/Ambev.DeveloperEvaluation.Domain/Repositories/ICartRepository.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ICartRepository.cs
@@ -5,6 +5,16 @@ namespace Ambev.DeveloperEvaluation.Domain.Repositories
     public interface ICartRepository
     {
         Task<Cart> CreateAsync(Cart cart, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Retrieves a cart by ID
+        /// </summary>
+        /// <param name="id">Cart ID</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The cart if found, otherwise null</returns>
+        Task<Cart?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+
     }
 
 }
