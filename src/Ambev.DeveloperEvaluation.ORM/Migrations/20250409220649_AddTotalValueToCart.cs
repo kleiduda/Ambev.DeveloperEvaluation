a202864714718bb45476d282abe9d2c758279b50
Kleiton <kleiduda@gmail.com>
2025-04-09 19:09:39 -0300

    feat(migrations): cria migration AddTotalValueToCart para persistência do valor total do carrinho

---
+++ src/Ambev.DeveloperEvaluation.ORM/Migrations/20250409220649_AddTotalValueToCart.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalValueToCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalValue",
+                table: "Carts",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalValue",
+                table: "Carts");
+        }
+    }
+}
