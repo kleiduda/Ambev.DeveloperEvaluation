e402e801e37122e817ace30284a65e6cbed294e1
Kleiton <kleiduda@gmail.com>
2025-04-09 15:37:52 -0300

    feat(application): adiciona GetCategoriesQuery e handler para retornar categorias únicas

---
+++ src/Ambev.DeveloperEvaluation.Application/Products/GetCategories/GetCategoriesHandler.cs
@@ -0,0 +1,21 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.GetCategories
+{
+    public class GetCategoriesHandler : IRequestHandler<GetCategoriesQuery, List<string>>
+    {
+        private readonly IProductRepository _repository;
+
+        public GetCategoriesHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<string>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetAllCategoriesAsync(cancellationToken);
+        }
+    }
+
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Products/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Products.GetCategories
+{
+    public record GetCategoriesQuery() : IRequest<List<string>>;
+
+}
