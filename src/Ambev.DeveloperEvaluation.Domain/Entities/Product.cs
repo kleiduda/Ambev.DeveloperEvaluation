ed5f8437b45065ed004db8fb2050265f56351eba
Kleiton <kleiduda@gmail.com>
2025-04-09 13:40:19 -0300

    feat(domain): adiciona entidade Product com objeto de valor Rating

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Title { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+
+        public Rating Rating { get; set; } = new();
+    }
+
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/ValueObjects/Rating.cs
@@ -0,0 +1,8 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.ValueObjects
+{
+    public class Rating
+    {
+        public double Rate { get; set; }
+        public int Count { get; set; }
+    }
+}
