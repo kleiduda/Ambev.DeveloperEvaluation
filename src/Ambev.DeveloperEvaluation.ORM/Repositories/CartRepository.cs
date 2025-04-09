cf246e4da535e452844965a33b69c5dbe04cfb28
Kleiton <kleiduda@gmail.com>
2025-04-09 16:12:28 -0300

    feat(infrastructure): implementa persistência de Cart com produtos associados

---
+++ src/Ambev.DeveloperEvaluation.ORM/Repositories/CartRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly DefaultContext _context;
+
+        public CartRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Cart> CreateAsync(Cart cart, CancellationToken cancellationToken = default)
+        {
+            await _context.Carts.AddAsync(cart, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return cart;
+        }
+    }
+
+}
