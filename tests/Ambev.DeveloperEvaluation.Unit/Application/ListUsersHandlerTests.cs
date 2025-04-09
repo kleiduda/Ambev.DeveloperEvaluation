410d9ad3c0d67f7a6019b03c26d445a2ca77d285
Kleiton <kleiduda@gmail.com>
2025-04-09 13:01:16 -0300

    test(unit): adiciona cenários de lista vazia e paginação parcial ao ListUsersHandler

--- tests/Ambev.DeveloperEvaluation.Unit/Application/ListUsersHandlerTests.cs
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/ListUsersHandlerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Users.GetUsers;
+using Ambev.DeveloperEvaluation.Domain.Entities;
 using Ambev.DeveloperEvaluation.Domain.Repositories;
 using Ambev.DeveloperEvaluation.Unit.Domain.Entities.TestData;
 using FluentAssertions;
@@ -40,6 +41,55 @@ namespace Ambev.DeveloperEvaluation.Unit.Application
             await _userRepository.Received(1)
                 .GetPaginatedAsync(1, 10, "username asc", Arg.Any<CancellationToken>());
         }
+
+        [Fact(DisplayName = "Given valid parameters When no users exist Then returns empty result")]
+        public async Task Handle_EmptyResult_ReturnsEmptyList()
+        {
+            // Given
+            var command = new ListUsersCommand(1, 10, "username asc");
+
+            _userRepository.GetPaginatedAsync(1, 10, "username asc", Arg.Any<CancellationToken>())
+                .Returns((new List<User>(), totalItems: 0));
+
+            // When
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Then
+            result.Should().NotBeNull();
+            result.Items.Should().BeEmpty();
+            result.TotalItems.Should().Be(0);
+            result.CurrentPage.Should().Be(1);
+            result.PageSize.Should().Be(10);
+
+            await _userRepository.Received(1)
+                .GetPaginatedAsync(1, 10, "username asc", Arg.Any<CancellationToken>());
+        }
+
+        [Fact(DisplayName = "Given second page When listing users Then returns correct page data")]
+        public async Task Handle_Pagination_ReturnsCorrectPage()
+        {
+            // Given
+            var command = new ListUsersCommand(2, 10, "username asc");
+            var users = Enumerable.Range(0, 3).Select(_ => UserTestData.GenerateValidUser()).ToList();
+
+            _userRepository.GetPaginatedAsync(2, 10, "username asc", Arg.Any<CancellationToken>())
+                .Returns((users, totalItems: 13));
+
+            // When
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Then
+            result.Should().NotBeNull();
+            result.Items.Should().HaveCount(3);
+            result.TotalItems.Should().Be(13);
+            result.CurrentPage.Should().Be(2);
+            result.PageSize.Should().Be(10);
+
+            await _userRepository.Received(1)
+                .GetPaginatedAsync(2, 10, "username asc", Arg.Any<CancellationToken>());
+        }
+
+
     }
 
 }
