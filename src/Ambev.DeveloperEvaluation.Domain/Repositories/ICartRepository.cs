9250b76d66b61c9c54c0b8aaadf3133b133477e6
Kleiton <kleiduda@gmail.com>
2025-04-09 16:10:53 -0300

    feat(domain): define interface ICartRepository com método CreateAsync

---
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ICartRepository.cs
@@ -0,0 +1,10 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Domain.Repositories
+{
+    public interface ICartRepository
+    {
+        Task<Cart> CreateAsync(Cart cart, CancellationToken cancellationToken);
+    }
+
+}
