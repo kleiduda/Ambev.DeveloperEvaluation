e7466dd8e9de3650bc4d53d4bf96935ec6a7dc18
Kleiton <kleiduda@gmail.com>
2025-04-09 21:32:59 -0300

    feat(application): estende ISaleRepository com método de busca paginada com filtros

--- src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Repositories/ISaleRepository.cs
@@ -5,6 +5,19 @@ namespace Ambev.DeveloperEvaluation.Domain.Repositories
     public interface ISaleRepository
     {
         Task<Sale> CreateAsync(Sale sale, CancellationToken cancellationToken);
+        Task<(List<Sale> Items, int TotalItems)> GetPaginatedAsync(
+        int page,
+        int pageSize,
+        string? orderBy,
+        string? clienteNome,
+        string? numeroVenda,
+        DateTime? minDataVenda,
+        DateTime? maxDataVenda,
+        decimal? minValorTotal,
+        decimal? maxValorTotal,
+        bool? cancelada,
+        CancellationToken cancellationToken);
+
     }
 
 }
