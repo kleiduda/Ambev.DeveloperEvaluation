a3b0b92aeffdc05862567d84d71604f70e7fa790
Kleiton <kleiduda@gmail.com>
2025-04-10 01:23:26 -0300

    test(products): estende classe de test data

--- tests/Ambev.DeveloperEvaluation.Unit/Application/Products/TestData/CreateProductHandlerTestData.cs
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/Products/TestData/CreateProductHandlerTestData.cs
@@ -1,11 +1,13 @@
 ﻿using Ambev.DeveloperEvaluation.Application.Products;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.ValueObjects;
 using Bogus;
 
 namespace Ambev.DeveloperEvaluation.Unit.Application.Products.TestData
 {
     public static class CreateProductHandlerTestData
     {
-        private static readonly Faker<CreateProductCommand> ProductFaker = new Faker<CreateProductCommand>("pt_BR")
+        private static readonly Faker<CreateProductCommand> ProductCommandFaker = new Faker<CreateProductCommand>("pt_BR")
             .RuleFor(p => p.Title, f => f.Commerce.ProductName())
             .RuleFor(p => p.Price, f => f.Random.Decimal(10, 10000))
             .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
@@ -17,7 +19,22 @@ namespace Ambev.DeveloperEvaluation.Unit.Application.Products.TestData
                 Count = f.Random.Int(0, 999)
             });
 
-        public static CreateProductCommand GenerateValidCommand() => ProductFaker.Generate();
+        private static readonly Faker<Product> ProductFaker = new Faker<Product>("pt_BR")
+            .RuleFor(p => p.Id, f => Guid.NewGuid())
+            .RuleFor(p => p.Title, f => f.Commerce.ProductName())
+            .RuleFor(p => p.Price, f => f.Random.Decimal(10, 10000))
+            .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+            .RuleFor(p => p.Category, f => f.Commerce.Categories(1).First())
+            .RuleFor(p => p.Image, f => f.Image.PicsumUrl())
+            .RuleFor(p => p.Rating, f => new Rating
+            {
+                Rate = (double)(decimal)f.Random.Double(0, 5),
+                Count = f.Random.Int(0, 999)
+            });
+
+        public static CreateProductCommand GenerateValidCommand() => ProductCommandFaker.Generate();
+        public static Product GenerateValidProduct() => ProductFaker.Generate();
     }
+
 
 }
